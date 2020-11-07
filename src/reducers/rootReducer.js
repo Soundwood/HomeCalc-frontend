7c6cc185283a696ef2c8182b7e08bf4e772d9049
Soundwood <j.benhogan@gmail.com>
2020-11-07 08:02:25 -0600

    add loginform and signupform

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { scenariosReducer } from "./scenariosReducer";
 import { usersReducer } from "./usersReducer";
 import { currentUser } from './currentUser';
 import loginForm from './loginForm'
+import signupForm from './signupForm'
 
 export const rootReducer = combineReducers({
     scenarios: scenariosReducer, 
     users: usersReducer,
     currentUser,
-    loginForm
+    loginForm,
+    signupForm
 //   users: usersReducer,
 //   courses: coursesReducer,
 //   beers: beerReducer,
