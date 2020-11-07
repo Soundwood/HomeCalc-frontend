686eee632aa366b6adb5bd054443322cacf0ccb1
Soundwood <j.benhogan@gmail.com>
2020-11-07 08:04:39 -0600

    remove unnused

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { rootReducer } from './reducers/rootReducer'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
