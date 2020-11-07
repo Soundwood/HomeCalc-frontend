3d1ad19f4ac3d142db6cfb6000d63aab806b61d0
Soundwood <j.benhogan@gmail.com>
2020-11-07 07:51:35 -0600

    css changes

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser'
 
 const Logout = ({ logout }) => {
 
     return (
-        <form onSubmit={logout}>
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            logout()}
+        }>
             <input type="submit" value="Logout"/>
         </form>
     )
