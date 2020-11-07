13003bd160b1c8d4dd93f85f09a0d78125dbbcee
Soundwood <j.benhogan@gmail.com>
2020-11-07 08:00:05 -0600

    add rest of fields

--- src/components/ScenarioForm.js
+++ src/components/ScenarioForm.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addScenario } from '../actions/scenariosActions'
+import { getCurrentUser } from "../actions/currentUser"
 
 class ScenarioForm extends Component {
-    
+
     state = {
+        user: this.props.currentUser,
         net_income: '',
         income_after_tax: '',
         monthly_debt: '',
@@ -30,14 +32,35 @@ class ScenarioForm extends Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Net Income:</label>
-                    <input type='text' value={this.state.net_income} onChange={this.handleChange} name='net_income'/>
-                    <br />
+                    <label>Net Income (per year):</label>
+                    <input type='text' value={this.state.net_income} onChange={this.handleChange} name='net_income'/><br/>
+                    <label>Income After Tax (per year):</label>
+                    <input type='text' value={this.state.income_after_tax} onChange={this.handleChange} name='income_after_tax'/><br/>
+                    <label>Monthly Recurring Debt:</label>
+                    <input type='text' value={this.state.monthly_debt} onChange={this.handleChange} name='monthly_debt'/><br/>
+                    <label>Credit Score:</label>
+                    <select value={this.state.credit_score} onChange={this.handleChange} name='credit_score'>
+                        <option value="great">Great - 720-850</option>
+                        <option value="good">Good - 690-719</option> 
+                        <option value="fair">Fair - 630-689</option>
+                        <option value="poor">Poor - 300-629</option>
+                    </select><br/>
+                    <label>Home Downpayment:</label>
+                    <input type='text' value={this.state.downpayment} onChange={this.handleChange} name='downpayment'/><br/>
+                    <label>City:</label>
+                    <input type='text' value={this.state.city} onChange={this.handleChange} name='city'/><br/>
+                    <label>State:</label>
+                    <input type='text' value={this.state.state} onChange={this.handleChange} name='state'/><br/>
                     <input type='submit' value='Evaluate Scenario' />
                 </form>
             </div>
         )
     }
 }
+const mapStateToProps = ({currentUser}) => {
+    return {
+        currentUser
+    }
+}
 
-export default connect( null, addScenario )(ScenarioForm)
+export default connect(mapStateToProps, { getCurrentUser, addScenario } )(ScenarioForm)
