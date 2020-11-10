054b8c96e82a50d40c80b5f74b75d7f0e09f6007
Soundwood <j.benhogan@gmail.com>
2020-11-10 09:19:28 -0600

    flesh out content

--- src/components/ScenarioCard.js
+++ src/components/ScenarioCard.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { deleteScenario } from '../actions/scenarios'
 
-const ScenarioCard = ({ scenario }) => {
+const ScenarioCard = ({ scenario, deleteScenario, history}) => {
     return (
-        <p>{scenario.net_income}</p>
+        <>
+            <Link to={`/scenarios/${scenario.id}`}>
+                <h5>Net Income: {scenario.net_income}</h5>
+                <h5>Income After Tax: {scenario.income_after_tax}</h5>
+                <h5>Monthly Debt: {scenario.monthly_debt}</h5>
+                <h5>Credit Score: {scenario.credit_score}</h5>
+                <h5>Downpayment: {scenario.downpayment}</h5>
+                <h5>City: {scenario.city}</h5>
+                <h5>State: {scenario.state}</h5>
+                <h4>Click here to see an analysis of THIS scenario</h4>
+
+                <br/>
+            </Link>
+            <button onClick={() => deleteScenario(scenario, history)}>Delete</button>
+        </>
     )
 }
 
-export default ScenarioCard
+export default connect(null, {deleteScenario})(ScenarioCard)
--- src/components/ScenarioForm.js
+++ src/components/ScenarioForm.js
@@ -1,66 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
-import { addScenario } from '../actions/scenariosActions'
-import { getCurrentUser } from "../actions/currentUser"
+import { updateScenarioForm } from '../actions/scenarioForm'
+import { createScenario } from '../actions/scenarios'
 
-class ScenarioForm extends Component {
-
-    state = {
-        user: this.props.currentUser,
-        net_income: '',
-        income_after_tax: '',
-        monthly_debt: '',
-        credit_score: '',
-        downpayment: '',
-        state: '',
-        city: '',
-        prop_tax_rate_id: '',
-        mortg_interest_rate_id: ''
-    }
-    handleChange = (e) => {
+const ScenarioForm = ({ formData, updateScenarioForm, user_id, scenario, createScenario, editMode, tax_by_state, mortg_rates, history}) => {
+    const { net_income, income_after_tax, monthly_debt, credit_score, downpayment, city, state } = formData
+    
+    const handleChange = (e) => {
         const { name, value } = e.target
-        this.setState({
-            [name]: value
-        })
+        // Make sure this makes sense ie its a number
+        updateScenarioForm(name, value)
     }
-    handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault()
-        debugger
-        this.props.addScenario(this.state)
+        const prop_tax_rate_id = tax_by_state.filter(function(st) {return st.state === state})[0].id
+        const mortg_interest_rate_id = mortg_rates.filter(function(rate) {return rate.credit_score === credit_score})[0].id
+        createScenario({...formData, user_id, prop_tax_rate_id, mortg_interest_rate_id}, history)
     }
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Net Income (per year):</label>
-                    <input type='text' value={this.state.net_income} onChange={this.handleChange} name='net_income'/><br/>
-                    <label>Income After Tax (per year):</label>
-                    <input type='text' value={this.state.income_after_tax} onChange={this.handleChange} name='income_after_tax'/><br/>
-                    <label>Monthly Recurring Debt:</label>
-                    <input type='text' value={this.state.monthly_debt} onChange={this.handleChange} name='monthly_debt'/><br/>
-                    <label>Credit Score:</label>
-                    <select value={this.state.credit_score} onChange={this.handleChange} name='credit_score'>
-                        <option value="great">Great - 720-850</option>
-                        <option value="good">Good - 690-719</option> 
-                        <option value="fair">Fair - 630-689</option>
-                        <option value="poor">Poor - 300-629</option>
-                    </select><br/>
-                    <label>Home Downpayment:</label>
-                    <input type='text' value={this.state.downpayment} onChange={this.handleChange} name='downpayment'/><br/>
-                    <label>City:</label>
-                    <input type='text' value={this.state.city} onChange={this.handleChange} name='city'/><br/>
-                    <label>State:</label>
-                    <input type='text' value={this.state.state} onChange={this.handleChange} name='state'/><br/>
-                    <input type='submit' value='Evaluate Scenario' />
-                </form>
-            </div>
-        )
-    }
+
+    return (
+        <article className="post">
+            <form onSubmit={handleSubmit}>
+                <label>Net Income (per year):</label>
+                <input type='text' value={net_income} onChange={handleChange} name='net_income'/><br/>
+                <label>Income After Tax (per year):</label>
+                <input type='text' value={income_after_tax} onChange={handleChange} name='income_after_tax'/><br/>
+                <label>Monthly Recurring Debt:</label>
+                <input type='text' value={monthly_debt} onChange={handleChange} name='monthly_debt'/><br/>
+                <label>Credit Score:</label>
+                <select value={credit_score} onChange={handleChange} name='credit_score'>
+                    <option value="great">Great - 720-850</option>
+                    <option value="good">Good - 690-719</option> 
+                    <option value="fair">Fair - 630-689</option>
+                    <option value="poor">Poor - 300-629</option>
+                </select><br/>
+                <label>Home Downpayment:</label>
+                <input type='text' value={downpayment} onChange={handleChange} name='downpayment'/><br/>
+                <label>City:</label>
+                <input type='text' value={city} onChange={handleChange} name='city'/><br/>
+                <label>State:</label>
+                <input type='text' value={state} onChange={handleChange} name='state'/><br/>
+                <input type='submit' value='Evaluate Scenario' />
+            </form>
+        </article>
+    )
 }
-const mapStateToProps = ({currentUser}) => {
+const mapStateToProps = state => {
+    const userId = state.currentUser ? state.currentUser.id : ""
     return {
-        currentUser
+        formData: state.scenario,
+        user_id: userId,
+        tax_by_state: state.prop_tax_rates,
+        mortg_rates: state.mortg_int_rates
     }
 }
 
-export default connect(mapStateToProps, { getCurrentUser, addScenario } )(ScenarioForm)
+export default connect(mapStateToProps, { updateScenarioForm, createScenario } )(ScenarioForm)
